98633bc29d0ce83ac1960069e1c5fa6207b97eb6
Pandragona <asiuspospolitus@gmail.com>
2019-03-03 18:39:42 +0100

    basic front end

--- app/src/main/java/com/example/weathercast/MainActivity.java
+++ app/src/main/java/com/example/weathercast/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.weathercast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText typeTheCity;
+    private Button search;
+    private TextView temperature;
+    private TextView feltTemperature;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search = findViewById(R.id.button);
+        typeTheCity = findViewById(R.id.editText);
+        temperature = findViewById(R.id.textView4);
+        feltTemperature = findViewById(R.id.feelTemp);
+
+
     }
 }
